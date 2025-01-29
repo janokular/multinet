444b437ad6200acbedb7fbe74174eedd8111d651
janokular <janekokularczyk@gmail.com>
2025-01-29 15:33:51 +0100

    staged changes

--- multinet-status.sh
+++ multinet-status.sh
@@ -4,12 +4,14 @@
 
 SERVER_FILE='./servers'
 
+# Check if SERVER_FILE exists and is a file
 if [[ ! -e "${SERVER_FILE}" ]]
 then
   echo "Cannot open ${SERVER_FILE}" >&2
   exit 1
 fi
 
+# Ping every server from the SERVER_FILE
 for SERVER in $(cat ${SERVER_FILE})
 do
   echo "Pinging ${SERVER}"
